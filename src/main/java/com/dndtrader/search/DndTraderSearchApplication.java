098b09c0bd7b688be2550604645e0ec36b642481
RudAndr <rudenk.andr@gmail.com>
2025-04-15 00:05:18 +0300

    Initial commit

---
+++ src/main/java/com/dndtrader/search/DndTraderSearchApplication.java
@@ -0,0 +1,13 @@
+package com.dndtrader.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndTraderSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DndTraderSearchApplication.class, args);
+    }
+
+}
